8fa310c70efde2938a1c7d888963684485a4e77e
Amir javeed <amir@vaultspay.ae>
2024-08-26 01:08:11 +0400

    MainScreen Content Completed

--- workmanagerwithapicallandroom/src/main/java/com/example/workmanagerwithapicallandroom/presentation/MainActivity.kt
+++ workmanagerwithapicallandroom/src/main/java/com/example/workmanagerwithapicallandroom/presentation/MainActivity.kt
@@ -4,44 +4,98 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.workmanagerwithapicallandroom.presentation.ui.theme.WorkManagerJetpackComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WorkManagerJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.safeContentPadding()) {
+                    val viewModel = hiltViewModel<MainViewModel>()
+                    MainScreen(viewModel = viewModel) {
+                        viewModel.getQuote()
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorkManagerJetpackComposeTheme {
-        Greeting("Android")
+fun MainScreen(viewModel: MainViewModel, fetchQuote: () -> Unit) {
+    val uiState by viewModel.uiState.collectAsState()
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "Quotes") }, actions = {
+            IconButton(onClick = fetchQuote) {
+                Icon(imageVector = Icons.Filled.Refresh, contentDescription = "")
+            }
+        })
+    }) {
+        if (uiState.data.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize(), contentAlignment = Alignment.CenterStart
+            ) {
+                Text(text = "Nothing Found")
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            ) {
+                items(uiState.data) {
+                    Card(modifier = Modifier.padding(8.dp)) {
+                        Column {
+                            Text(text = it.quote)
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(text = "Author: " + it.author)
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Row(
+                                modifier = Modifier.fillParentMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(text = it.time.toString())
+                                Text(text = it.workType)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
