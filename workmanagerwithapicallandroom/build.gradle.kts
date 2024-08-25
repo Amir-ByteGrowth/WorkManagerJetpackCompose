298afa6e5ba3ad83c7ae81e20ad2e918c04235ca
Amir javeed <amir@vaultspay.ae>
2024-08-25 23:24:14 +0400

    room, hilt dependencies added

--- workmanagerwithapicallandroom/build.gradle.kts
+++ workmanagerwithapicallandroom/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.dagger)
+    kotlin("kapt")
 }
 
 android {
@@ -66,4 +68,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation(libs.work.manager)
+    implementation(libs.work.manager.dagger)
+    kapt(libs.work.manager.dagger.kapt)
+    implementation(libs.hilt.compose.navigation)
+
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.kapt)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson.convertor)
+
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
 }
