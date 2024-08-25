7602df2ab8108ea5dca7da8be4dbe88d09db3e8a
Amir javeed <amir@vaultspay.ae>
2024-08-26 00:37:49 +0400

    repository implementation done

--- workmanagerwithapicallandroom/src/main/java/com/example/workmanagerwithapicallandroom/data/repository/QuoteRepoImp.kt
+++ workmanagerwithapicallandroom/src/main/java/com/example/workmanagerwithapicallandroom/data/repository/QuoteRepoImp.kt
@@ -1,19 +1,50 @@
 package com.example.workmanagerwithapicallandroom.data.repository
 
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequest.Companion.MIN_PERIODIC_INTERVAL_MILLIS
+import androidx.work.WorkManager
+import com.example.workmanagerwithapicallandroom.data.local.QuoteDao
+import com.example.workmanagerwithapicallandroom.data.mappers.PeriodicWorker
+import com.example.workmanagerwithapicallandroom.data.worker.FetchWorker
 import com.example.workmanagerwithapicallandroom.domain.models.Quote
 import com.example.workmanagerwithapicallandroom.domain.repository.QuoteRepository
 import kotlinx.coroutines.flow.Flow
+import java.util.concurrent.TimeUnit
 
-class QuoteRepoImp :QuoteRepository {
+class QuoteRepoImp(
+    private val workManager: WorkManager,
+    private val quoteDao: QuoteDao,
+) : QuoteRepository {
     override fun getQuote() {
-        TODO("Not yet implemented")
+        val constraints =
+            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        val workRequest =
+            OneTimeWorkRequestBuilder<FetchWorker>().setConstraints(constraints).build()
+        workManager.enqueue(workRequest)
+
     }
 
-    override fun getAllQQuotes(): Flow<List<Quote>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAllQQuotes(): Flow<List<Quote>> = quoteDao.getAllQuotes()
 
     override fun setPeriodicWorkRequest() {
-        TODO("Not yet implemented")
+        val constraints =
+            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(
+            PeriodicWorker::class.java,
+            MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS
+        ).build()
+
+        workManager.enqueueUniquePeriodicWork(
+            "periodic_work",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            periodicWorkRequest
+        )
+
+
+
     }
 }
